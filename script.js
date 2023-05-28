d563fb99e66e7665edd437c2d9ba32b87a728dd3
CruzWin <javier1996.ec@gmail.com>
2023-05-29 00:49:23 +0200

    with animation

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-const as = document.querySelector("a:last-child");
-console.log(as.textContent);
+const contenido = document.querySelector(".content");
+const navbar = document.querySelector("nav");
+const logo = document.querySelector("i.fab");
+function hidden() {
+  contenido.classList.add("scale0");
+  navbar.classList.add("scale0");
+  logo.style.fontSize = "10em";
+}
+
+function show(...args) {
+  logo.style.fontSize = "";
+  // logo.style.removeProperty("fontSize");
+
+  args.forEach((etiqueta) => {
+    etiqueta.classList.remove("scale0");
+    etiqueta.classList.add("scale1");
+  });
+}
+
+hidden();
+addEventListener("load", () => {
+  setTimeout(show, 500, contenido, navbar);
+});
